7e73af57f64f792cfee54b37dd507d25f5d91a2d
mlunceford <mark7lunceford@gmail.com>
2019-08-22 18:49:49 -0600

    stuck

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -5,26 +5,36 @@
 //Create a variable called lovesCode and set it true or false (as a boolean) depending on if you love code or not. It's okay if you don't, yet
 
 // Code Here
-
+var lovesCode = true
 //////////////////STEP 2////////////////////
 //Create a variable called age that is your age as a number
 
 // Code Here
-
+var age = 30
 //////////////////STEP 3////////////////////
 //Create a variable called name that is your name as a string
 
 // Code Here
-
+var name = 'Mark'
 //////////////////STEP 4////////////////////
 //Create a variable called phrase that is initially set as an empty string
 
 // Code Here
-
+var phrase = 'I love code'
 //////////////////STEP 5////////////////////
 //Create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
 // Code Here
-
+if(lovesCode === true){
+        return phrase = 'I love code'
+    }else{
+        return phrase = 'I do not love to code'
+}
+// if (lovesCode === true) {
+//     document.body.style.backgroundColor = '#ceffef'
+//   } else {
+//     document.body.style.backgroundColor = '#ffb9ac'
+//   }
+  
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
